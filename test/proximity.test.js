bce379f2a5c43fe44188fa989400b3049e2ef4ff
Ansis Brammanis <brammanis@gmail.com>
2016-11-30 11:10:22 -0800

    use cover to make distance more accurate
    After loading the features from the index (but before loading them from
    the vector tiles) the features do not have a full geometry. They have a
    `carmen:center` property that has been used to calculate the distance to
    the feature.
    
    For large features, the center can be very far away from the nearest
    point in the feature. The Cover for the feature has additional spatial
    information: the x, y, z coordinates.
    
    This pr makes the distance to the feature be
    min(distanceToCenter, distanceToFurthestCornerOfCover)
    which should improve accuracy for large features.

--- test/proximity.test.js
+++ test/proximity.test.js
@@ -9,26 +9,27 @@ test('proximity.center2zxy', function(assert) {
     assert.end();
 });
 
-test('proximity._scoredist', function(assert) {
-    assert.equal(proximity._scoredist(1, 80.0), 0.5, '80.0 miles');
-    assert.equal(proximity._scoredist(1, 40.0), 1, '40.0 miles');
-    assert.equal(proximity._scoredist(1, 20.0), 2, '20.0 miles');
-    assert.equal(proximity._scoredist(1, 8.0),  5,   '8.0 miles');
-    assert.equal(proximity._scoredist(1, 5.0),  8,   '5.0 miles = break even pt');
-    assert.equal(proximity._scoredist(1, 4.0),  10,   '4.0 miles');
-    assert.equal(proximity._scoredist(1, 2.0),  20,   '2.0 miles');
-    assert.equal(proximity._scoredist(1, 1.0),  40,   '1.0 miles');
-    assert.equal(proximity._scoredist(1, 0.2),  200,  '0.2 miles');
-    assert.equal(proximity._scoredist(1, 0.1),  400,  '0.1 miles');
-    assert.equal(proximity._scoredist(1, 0.0),  400000, '0.0 miles');
+test('proximity.scoredist', function(assert) {
+    assert.equal(proximity.scoredist(80.0, 1), 0.5, '80.0 miles');
+    assert.equal(proximity.scoredist(40.0, 1), 1, '40.0 miles');
+    assert.equal(proximity.scoredist(20.0, 1), 2, '20.0 miles');
+    assert.equal(proximity.scoredist(8.0, 1),  5,   '8.0 miles');
+    assert.equal(proximity.scoredist(5.0, 1),  8,   '5.0 miles = break even pt');
+    assert.equal(proximity.scoredist(4.0, 1),  10,   '4.0 miles');
+    assert.equal(proximity.scoredist(2.0, 1),  20,   '2.0 miles');
+    assert.equal(proximity.scoredist(1.0, 1),  40,   '1.0 miles');
+    assert.equal(proximity.scoredist(0.2, 1),  200,  '0.2 miles');
+    assert.equal(proximity.scoredist(0.1, 1),  400,  '0.1 miles');
+    assert.equal(proximity.scoredist(0.0, 1),  400000, '0.0 miles');
     assert.end();
 });
 
-test('proximity.scoredist', function(assert) {
-    // lon/lat center
-    assert.equal(proximity.scoredist([0,0], [0,0], 1000), 4e8, 'll scoredist');
-    assert.equal(proximity.scoredist([0,0], [0.05,0], 1000), 11574.905, 'll scoredist');
-    assert.equal(proximity.scoredist([0,0], [2.00,0], 1000), 289.3726, 'll scoredist');
-    assert.equal(proximity.scoredist([0,0], [10.00,0], 1000), 57.8745, 'll scoredist');
+test('proximity.distance', function(assert) {
+    // uses distance to center when closer than furthest corner of cover
+    assert.equal(proximity.distance([0, 0], [0, 0], { x: 0, y: 0, zoom: 2 }), 0);
+    // uses distance to furthest corner of cover when closer than center
+    assert.equal(proximity.distance([-170, 0], [0, 0], { x: 0, y: 1, zoom: 2 }), 5946.081666100757);
+    // changing center does not change distance when it is further than the furthest corner of the cover
+    assert.equal(proximity.distance([-170, 0], [10, 0], { x: 0, y: 1, zoom: 2 }), 5946.081666100757);
     assert.end();
 });
