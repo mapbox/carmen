ed491a880d04fb172edc66b291779da735cd3e60
Matt Ficke <matt@mattficke.com>
2017-01-09 19:10:21 -0500

    spam the index to lower cover relevance

--- test/addfeature.test.js
+++ test/addfeature.test.js
@@ -6,30 +6,102 @@ var addFeature = require('../lib/util/addfeature');
 
 var conf = {
     country: new mem({maxzoom: 6}, function() {}),
+    region: new mem({maxzoom: 6}, function() {}),
+    place: new mem({maxzoom: 6}, function() {})
 };
 var c = new Carmen(conf);
+var tiles1 = [];
+var tiles2 = [];
+var tile1;
+var tile2;
+for (var k=27; k<33; k++) {
+    for (var l=27; l<33; l++) {
+        tile1 = '6/' + k + '/' + l;
+        tile2 = '6/' + (k - 6) + '/' + (l - 6);
+        tiles1.push(tile1);
+        tiles2.push(tile2);
+    }
+}
 
-tape('index batch', function(t) {
+tape('index batch country', function(t) {
     var docs = [];
     var country;
-    for (var i=1; i < 6; i++) {
+
+    country = {
+        id:1,
+        type: 'Feature',
+        properties: {
+            'carmen:text':'united states',
+            'carmen:zxy':tiles1,
+            'carmen:center':[0,0]
+        }
+    };
+    docs.push(country);
+
+    country = {
+        id:2,
+        type: 'Feature',
+        properties: {
+            'carmen:text':'united states minor outlying islands',
+            'carmen:zxy':tiles1,
+            'carmen:center':[0,0]
+        }
+    };
+    docs.push(country);
+
+    for (var i=3; i < 260; i++) {
         country = {
             id:i,
             type: 'Feature',
             properties: {
-                'carmen:text':'united states ' + i,
-                'carmen:zxy':['6/32/32'],
+                'carmen:text':'united arab emirates',
+                'carmen:zxy':['6/10/10'],
                 'carmen:center':[0,0]
             }
         };
         docs.push(country);
     }
+
     addFeature(conf.country, docs, t.end);
 });
 
+tape('index region', function(t) {
+    var region = {
+        id:1,
+        type: 'Feature',
+        properties: {
+            'carmen:text':'Texas,TX',
+            'carmen:zxy':tiles1,
+            'carmen:center':[0,0]
+        }
+    };
+    addFeature(conf.region, region, t.end);
+});
+
+tape('index place', function(t) {
+    var place = {
+        id:1,
+        type: 'Feature',
+        properties: {
+            'carmen:text':'Dallas',
+            'carmen:zxy':tiles1,
+            'carmen:center':[0,0]
+        }
+    };
+    addFeature(conf.place, place, t.end);
+});
+
 tape('query batched features', function(t) {
-    c.geocode('united states', null, function(err, res) {
-        t.equals(res.features.length, 5, "finds all batched features")
+    c.geocode('united', {allow_dupes: true}, function(err, res) {
+        console.log("Res", res);
+        t.equals(res.features.length, 5, "finds batched features")
+        t.end();
+    });
+});
+
+tape('check relevance', function(t) {
+    c.geocode('dallas texas united states', {}, function(err, res) {
+        // TODO
         t.end();
     });
 });
