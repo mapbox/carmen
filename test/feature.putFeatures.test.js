634f6e23b1994327b0867f831831061d2e423ca6
Matt Ficke <matt@mattficke.com>
2017-04-21 16:20:27 -0400

    Add features with colliding IDs to test

--- test/feature.putFeatures.test.js
+++ test/feature.putFeatures.test.js
@@ -49,6 +49,34 @@ tape('putFeatures', function(assert) {
                 coordinates: [360/64+0.001,0]
             }
         },
+        {
+            id: 11111222222183870,
+            type: 'Feature',
+            properties: {
+                'carmen:text': 'Dr Jekyll',
+                'carmen:center': [ 0, 0 ],
+                'carmen:zxy': ['6/32/32'],
+                'carmen:score': 1000
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [ 0, 0 ]
+            }
+        },
+        {
+            id: 6832527855771070,
+            type: 'Feature',
+            properties: {
+                'carmen:text': 'Mr Hyde',
+                'carmen:center': [ 0, 0 ],
+                'carmen:zxy': ['6/32/32'],
+                'carmen:score': 10
+            },
+            geometry: {
+                type: 'Point',
+                coordinates: [ 0, 0 ]
+            }
+        },
     ], function(err) {
         assert.ifError(err);
         assert.equal(source._shards.feature[1], '{"1":{"id":1,"type":"Feature","properties":{"carmen:text":"a","carmen:center":[0,0],"carmen:zxy":["6/32/32"]},"geometry":{"type":"Point","coordinates":[0,0]}},"1048577":{"id":1048577,"type":"Feature","properties":{"carmen:text":"c","carmen:center":[5.626,0],"carmen:zxy":["6/33/32"]},"geometry":{"type":"Point","coordinates":[5.626,0]}}}', 'has feature shard 1');
@@ -67,6 +95,13 @@ tape('getFeatureByCover', function(assert) {
 tape('getFeatureByCover', function(assert) {
     feature.getFeatureByCover(conf.source, { id:1, x:33, y:32 }, function(err, data) {
         assert.equal(data.id, 1048577);
+        assert.end();
+    });
+});
+
+tape('getFeatureByCover, collision', function(assert) {
+    feature.getFeatureByCover(conf.source, { id:187838, x:32, y:32 }, function(err, data) {
+        console.log("data", data)
         assert.end();
     });
 });
